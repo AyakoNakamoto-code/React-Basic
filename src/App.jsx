efadb19880be7b6f8617f7fb5604092eaa8a9fec
Ayako Nakamoto <109193998+AyakoNakamoto-code@users.noreply.github.com>
2022-08-01 14:13:17 +0900

    表示非表示ボタンと機能を追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,27 @@ import React, { useState } from "react";
 import ColorfullMessage from "./components/colorfullMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faseShowFlag, setFaseShowFlag] = useState(true);
+
   const onClickButtonUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaseShowFlag(!faseShowFlag);
+  };
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
       <ColorfullMessage color="pink" message="元気です！" />
       <button onClick={onClickButtonUp}>カウントアップボタン</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>On/Off</button>
       <p>{num}</p>
+      {/* faseShowFlagがtrueの時に表示する */}
+      {faseShowFlag && <p>(*´꒳`*)</p>}
     </>
   );
 };
