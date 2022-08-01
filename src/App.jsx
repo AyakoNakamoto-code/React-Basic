46a8a847e8914e8c57c2523289fe3f2576a271ec
Ayako Nakamoto <109193998+AyakoNakamoto-code@users.noreply.github.com>
2022-08-01 14:00:50 +0900

    App.jsxでカウントアップボタンを追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React, { useState } from "react";
 import ColorfullMessage from "./components/colorfullMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const onClickButtonUp = () => {
+    setNum(num + 1);
+  };
   const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
       <ColorfullMessage color="pink" message="元気です！" />
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickButtonUp}>カウントアップボタン</button>
       <p>{num}</p>
     </>
   );
