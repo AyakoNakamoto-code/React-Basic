c34c45c0ab128f681d5e6094f1d2f208013ffbbd
Ayako Nakamoto <109193998+AyakoNakamoto-code@users.noreply.github.com>
2022-08-01 13:57:46 +0900

    index.jsのエラー解消

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
-import ColorfullMessage from "./components/ColorfullMessage";
+import ColorfullMessage from "./components/colorfullMessage";
 
 const App = () => {
   const onClickButton = () => alert();
-  useState();
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
       <ColorfullMessage color="pink" message="元気です！" />
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
